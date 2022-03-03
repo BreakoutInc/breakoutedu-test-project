cc7ae7dc21993782b857b229a3c1436f512c81d6
yuliiarublenko <yuliia@breakoutedu.com>
2022-03-03 10:38:05 -0500

    finished District Admin Page, Added Hooks with screenshot function and teardown

---
+++ src/test/java/com/breakoutedu/step_definitions/Hooks.java
@@ -0,0 +1,35 @@
+package com.breakoutedu.step_definitions;
+import com.breakoutedu.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+
+    @Before
+    public void setupDriver() {
+        System.out.println("THIS IS FROM @Before inside hooks class");
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+            TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+        System.out.println("THIS IS FROM @After inside hooks class");
+        Driver.closeBrowser();
+
+    }
+}
