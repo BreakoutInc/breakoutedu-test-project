5fc9d7d513f8d1c4896318b7e126d2ebdb2da1cd
yuliiarublenko <yuliia@breakoutedu.com>
2022-04-06 23:35:01 -0400

    finished maven version update

--- src/test/java/com/breakoutedu/utility/Test.java
+++ src/test/java/com/breakoutedu/utility/Test.java
@@ -19,17 +19,17 @@ public class Test {
     public static void main(String[] args) {
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
-        BELoginPage loginPage = new BELoginPage();
-        Faker faker = new Faker();
-        StudentHomePage homePage = new StudentHomePage();
-        StudentMyGamesPage gamesPage = new StudentMyGamesPage();
+//        BELoginPage loginPage = new BELoginPage();
+//        Faker faker = new Faker();
+//        StudentHomePage homePage = new StudentHomePage();
+//        StudentMyGamesPage gamesPage = new StudentMyGamesPage();
 
 
         //student game play
-//        goTo("student");
+      goTo("student");
 //        loginPage.studentLoginWthBE(read("student1"),read("stud.psw"));
 
      // Driver.getDriver().quit();
-
+      //  System.out.println(read("file.name"));
     }
 }
