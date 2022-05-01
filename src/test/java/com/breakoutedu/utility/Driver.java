ead948a17673d8720f81c366ceb5aeee34f45d62
Chris <chris9owen@gmail.com>
2022-04-30 19:41:15 -0600

    add chromium support

--- src/test/java/com/breakoutedu/utility/Driver.java
+++ src/test/java/com/breakoutedu/utility/Driver.java
@@ -2,9 +2,7 @@ package com.breakoutedu.utility;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.Capabilities;
 
 public class Driver {
 
@@ -14,77 +12,46 @@ public class Driver {
     private Driver(){ }
 
     public static WebDriver getDriver(){
-        // read the browser type you want to launch from properties file
         String browserName = ConfigReader.read("browser") ;
-
         if(obj == null){
-
-            // according to browser type set up driver correctly
-            switch (browserName ){
-                case "chrome" :
-                    WebDriverManager.chromedriver().setup();
-                    obj = new ChromeDriver();
-                    break;
-                case "firefox" :
-                    WebDriverManager.firefoxdriver().setup();
-                    obj = new FirefoxDriver();
-                    break;
-                case "safari":
-                    WebDriverManager.safaridriver().setup();
-                    obj = new SafariDriver();
-                    break;
-                // other browsers omitted
-                default:
-                    obj = null ;
-                    System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
-            }
-            return obj ;
-
-
-
+            obj = getDriverBase(browserName);
         }else{
-            return obj ;
-
+            return obj;
         }
-
     }
 
     public static WebDriver getDriver2(){
-        // read the browser type you want to launch from properties file
         String browserName = ConfigReader.read("browser2") ;
-
         if(obj2 == null){
-
-            // according to browser type set up driver correctly
-            switch (browserName ){
-                case "chrome" :
-                    WebDriverManager.chromedriver().setup();
-                    obj2 = new ChromeDriver();
-                    break;
-                case "firefox" :
-                    WebDriverManager.firefoxdriver().setup();
-                    obj2 = new FirefoxDriver();
-                    break;
-                case "safari":
-                    WebDriverManager.safaridriver().setup();
-                    obj2 = new SafariDriver();
-                    break;
-                // other browsers omitted
-                default:
-                    obj2 = null ;
-                    System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
-            }
-            return obj2 ;
-
-
-
+            obj2 = getDriverBase(browserName);
         }else{
-            return obj2 ;
-
+            return obj2;
         }
-
     }
 
+    private static WebDriver getDriverBase(String browserName){
+        Webdriver newDriver;
+        // according to browser type set up driver correctly
+        switch (browserName ){
+            case "chrome" :
+                newDriver = WebDriverManager.chromedriver().create();
+                break;
+            case "firefox" :
+                newDriver = WebDriverManager.firefoxdriver().create();
+                break;
+            case "safari":
+                newDriver = WebDriverManager.safaridriver().create();
+                break;
+            case "chromium":
+                newDriver = WebDriverManager.chromiumdriver().create();
+                break;
+            // other browsers omitted
+            default:
+                newDriver = null ;
+                System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
+        }
+        return newDriver;
+    }
 
     public static void closeBrowser(){
         if(obj != null ){
@@ -92,6 +59,10 @@ public class Driver {
             // when ask for it again , it gives us not quited fresh driver
             obj = null ;
         }
-
+        if(obj2 != null ){
+            obj2.quit();
+            // when ask for it again , it gives us not quited fresh driver
+            obj2 = null ;
+        }
     }
 }
